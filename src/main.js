7bea04a29438533f589c361ea11e91bd48b96426
fanquanming <2836564875@qq.com>
2020-12-02 11:17:40 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/global.css';
+import axios from 'axios'
+Vue.prototype.$http =axios
+//配置请求的根路径
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 Vue.config.productionTip = false
-
+//使用ElementUI插件
+Vue.use(ElementUI)
 new Vue({
   router,
   store,
